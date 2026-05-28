e61d50e7c411852eedcd711a1593ab293efa6c6e
vitalin303 <vitalin303@gmail.com>
2026-05-28 21:51:48 +0700

    Add menu options for DOTZ AI BOT

---
+++ src/menu.js
@@ -0,0 +1,20 @@
+const menu = `
+🤖 DOTZ AI BOT
+
+📌 COMMAND:
+
+/chat teks
+/img prompt
+/tts teks
+/menu
+/owner
+/bc teks
+
+📌 CONTOH:
+
+/chat halo
+/img anime cowo keren
+/tts halo semuanya
+`
+
+module.exports = menu
